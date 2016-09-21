6005725866531311ee4e94df0db8b2516b28199e
DennP7791 <cookie070791@gmail.com>
2016-09-21 07:24:56 +0200

    QRVIdeoUserView

---
+++ Assets/Scripts/SupportScripts/QRVideoUserView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class QRVideoUserView
+{
+    public int VideoId;
+    public int UserId;
+    public DateTime ViewDate;
+
+    public QRVideoUserView(int videoId, int userId, DateTime viewDate)
+    {
+        VideoId = videoId;
+        UserId = userId;
+        ViewDate = viewDate;
+    }
+}
